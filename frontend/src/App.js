2b6e132ab994efcf3d9f61401b8df84d6b8b31eb
SirenFong <audition250601@gmail.com>
2024-02-27 12:31:58 +0700

    Thêm comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,6 +43,7 @@ const App = () => {
             <Route path="/best-selling" element={<BestSellingPage />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/faq" element={<FAQPage />} />
+            {/**Bọc ProfilePage bên trong ProtectedRout để kiểm tra đăng nhập */}
             <Route
               path="/profile"
               element={
